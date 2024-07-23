3080f1c7fefebbcc98727ea62c9f971c93c5462d
Thorsten Fischer <tf-git@tfsystem.de>
2024-07-23 19:24:56 +0200

    Beschreibung der Änderungen

---
+++ src/file1.c
@@ -0,0 +1,7 @@
+// file1.c
+#include <stdio.h>
+
+void function1() {
+    printf("This is function1\n");
+}
+
---
+++ src/file2.c
@@ -0,0 +1,7 @@
+// file2.c
+#include <stdio.h>
+
+void function2() {
+    printf("This is function2\n");
+}
+
